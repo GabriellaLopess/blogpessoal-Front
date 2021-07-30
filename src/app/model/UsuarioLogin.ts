0988f51cc2d0e4bf2c459e2aae1c61f84050ea1e
Gabriella Lopes <gabriellalopes0630@gmail.com>
2021-07-29 21:47:13 -0300

    Rotas de entrar e cadastrar criadas

---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,10 @@
+export class UsuarioLogin{
+
+    public id: number
+    public nome: string
+    public login: string
+    public senha: string
+    public token: string
+    public foto: string
+    public tipo: string
+  }
